d70218e3e77d6816f2df415847e6b51cc0b87f40
Nicolas <nicobsas+webdev@gmail.com>
2021-01-24 01:45:22 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Banner />
       <Row
         isLargeRow
