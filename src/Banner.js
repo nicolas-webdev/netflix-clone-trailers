67c69cf2ff9d3f155c0bade882b2bdc5d2b4342e
Nicolas <nicobsas+webdev@gmail.com>
2021-01-24 02:45:29 +0900

    fix videos not found

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import YouTube from "react-youtube";
 import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState();
+  const [featuredTrailer, setFeaturedTrailer] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -23,6 +25,25 @@ function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
 
+  const opts = {
+    height: "448",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  function handleClick(movie) {
+    if (featuredTrailer) {
+      setFeaturedTrailer("");
+    } else {
+      const urlParams = new URLSearchParams(
+        new URL("https://www.youtube.com/watch?v=lqRQ5Y6OYi4").search
+      );
+      setFeaturedTrailer(urlParams.get("v"));
+    }
+  }
+
   return (
     <header
       className="banner"
@@ -32,20 +53,29 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+      {featuredTrailer ? (
+        <YouTube videoId={featuredTrailer} opts={opts} />
+      ) : (
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
 
-        <div className="banner__butons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+          <div className="banner__butons">
+            <button
+              onClick={() => handleClick(movie)}
+              className="banner__button"
+            >
+              Play
+            </button>
+            <button className="banner__button">My List</button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+          </h1>
         </div>
-
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 150)}
-        </h1>
-      </div>
+      )}
       <div className="banner--fadeBottom" />
     </header>
   );
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, isLargeRow, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -16,6 +19,36 @@ function Row({ title, isLargeRow, fetchUrl }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log(movie?.title, movie?.name, movie?.original_name);
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+          const urlParams = new URLSearchParams(
+            new URL("https://www.youtube.com/watch?v=lqRQ5Y6OYi4").search
+          );
+          console.log(urlParams);
+          setTrailerUrl(urlParams.get("v"));
+        });
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className="row">
@@ -32,9 +65,11 @@ function Row({ title, isLargeRow, fetchUrl }) {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
