e275a8db2d692e40088da1677bcc81dc690d827c
Nicolas <nicobsas+webdev@gmail.com>
2021-01-24 00:08:29 +0900

    finish all row components

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "05d8521c408e540d2159defe15abae06";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
